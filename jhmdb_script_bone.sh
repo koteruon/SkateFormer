14bb0db988abbfe2f0f05c8dbc855b93774401b5
Chaoen <calvin19961211@gmail.com>
2025-05-28 03:37:22 +0800

    增加script

---
+++ jhmdb_script_bone.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CONFIG_FILE="./config/train/jhmdb/SkateFormer_b.yaml"
+
+i=2
+while [ $i -lt 100 ]
+do
+  # 格式化 OUTPUT_DIR 為四位數 (0000 ~ 9999)
+  OUTPUT_NUM=$(printf "%02d" $i)
+
+  # 修改 OUTPUT_DIR 的最後數字
+  sed -i "s|seed: .*|seed: ${i}|" "$CONFIG_FILE"
+  sed -i "s|work_dir: .*|work_dir: ./work_dir/jhmdb/SkateFormer_j_2D_20250528_seed_${OUTPUT_NUM}/|" "$CONFIG_FILE"
+
+  # 執行 python 指令，seed 直接使用數值格式
+  python main.py --config "$CONFIG_FILE"
+
+  # 增加計數
+  i=$((i + 1))
+done
