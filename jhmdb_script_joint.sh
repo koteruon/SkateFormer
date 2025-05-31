1bb2f85fa5214ab57e6dfdd1234c619a5d9e858a
Chaoen <calvin19961211@gmail.com>
2025-05-31 23:06:00 +0800

    joint 200epoch

--- jhmdb_script_joint.sh
+++ jhmdb_script_joint.sh
@@ -2,7 +2,7 @@
 
 CONFIG_FILE="./config/train/jhmdb/SkateFormer_j.yaml"
 
-i=86
+i=1
 while [ $i -lt 100 ]
 do
   # 格式化 OUTPUT_DIR 為四位數 (0000 ~ 9999)
@@ -10,7 +10,7 @@ do
 
   # 修改 OUTPUT_DIR 的最後數字
   sed -i "s|seed: .*|seed: ${i}|" "$CONFIG_FILE"
-  sed -i "s|work_dir: .*|work_dir: ./work_dir/jhmdb/SkateFormer_j_2D_20250521_seed_${OUTPUT_NUM}/|" "$CONFIG_FILE"
+  sed -i "s|work_dir: .*|work_dir: ./work_dir/jhmdb/SkateFormer_j_2D_20250531_seed_${OUTPUT_NUM}/|" "$CONFIG_FILE"
 
   # 執行 python 指令，seed 直接使用數值格式
   python main.py --config "$CONFIG_FILE"
