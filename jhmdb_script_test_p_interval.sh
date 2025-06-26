cc48a124164f6f0872c134025c0c053ac475b658
Chaoen <calvin19961211@gmail.com>
2025-06-26 15:24:43 +0800

    嘗試修改腳本

--- jhmdb_script_test_p_interval.sh
+++ jhmdb_script_test_p_interval.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONFIG_FILE="./config/train/jhmdb/SkateFormer_b.yaml"
+CONFIG_FILE="./config/train/jhmdb/SkateFormer_j.yaml"
 
 for intra_p in $(seq 0.50 0.05 1.00)
 do
@@ -8,7 +8,7 @@ do
   OUTPUT_NUM=$(printf "%03d" $(echo "$intra_p * 100" | bc | cut -d. -f1))
 
   # 修改 work_dir
-  sed -i "s|work_dir: .*|work_dir: ./work_dir/jhmdb/SkateFormer_b_2D_20250320_${OUTPUT_NUM}/|" "$CONFIG_FILE"
+  sed -i "s|work_dir: .*|work_dir: ./work_dir/jhmdb/SkateFormer_j_2D_20250602_${OUTPUT_NUM}/|" "$CONFIG_FILE"
 
   # 修改 intra_p
   sed -i "s|  p_interval: .*|  p_interval: [${intra_p}, 1]|" "$CONFIG_FILE"
